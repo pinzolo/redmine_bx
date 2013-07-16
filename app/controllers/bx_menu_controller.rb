d8c5d011b55342d84bc63bda340386a7c87f2e9d
pinzolo <pinzolo@gmail.com>
2013-07-17 00:42:38 +0900

    メニューページを作成 refs #123

--- app/controllers/bx_menu_controller.rb
+++ app/controllers/bx_menu_controller.rb
@@ -1,7 +1,16 @@
+# coding: utf-8
 class BxMenuController < ApplicationController
   unloadable
 
+  before_filter :find_project, :authorize
 
   def index
+  end
+
+  private
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
