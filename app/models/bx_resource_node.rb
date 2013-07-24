528a08610299f3b636145394a022fef476aec25a
pinzolo <pinzolo@gmail.com>
2013-07-24 23:30:11 +0900

    add roots scope to bx_resource_node refs #115

--- app/models/bx_resource_node.rb
+++ app/models/bx_resource_node.rb
@@ -1,3 +1,9 @@
+# coding: utf-8
 class BxResourceNode < ActiveRecord::Base
   unloadable
+
+  scope :roots, ->(project) {
+    project_id = project.is_a?(Project) ? project.id : project
+    where(:project_id => project_id, :parent_id => 0).order(:id)
+  }
 end
--- test/unit/bx_resource_node_test.rb
+++ test/unit/bx_resource_node_test.rb
@@ -1,9 +1,17 @@
+# coding: utf-8
 require File.expand_path('../../test_helper', __FILE__)
 
 class BxResourceNodeTest < ActiveSupport::TestCase
+  #fixtures :projects, :bx_resource_nodes
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  ## Replace this with your real tests.
+  #def test_roots_by_project_id
+  #  roots = BxResourceNode.roots(1)
+  #  assert roots.length, 2
+  #end
+
+  #def test_roots_by_project_instance
+  #  roots = BxResourceNode.roots(Project.find(1))
+  #  assert root.length
+  #end
 end
