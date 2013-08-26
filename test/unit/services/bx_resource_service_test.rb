f0f6d16f34b79c2ce89d9162dfbf9bf6f995576b
pinzlo <pinzolo@gmail.com>
2013-08-26 18:45:56 +0900

    add branch registration feature's testcases.

--- test/unit/services/bx_resource_service_test.rb
+++ test/unit/services/bx_resource_service_test.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../../../test_helper', __FILE__)
 
 class BxResourceServiceTest < ActiveSupport::TestCase
-  fixtures :projects, :issues, :users
+  fixtures :projects, :issues, :users, :bx_resource_nodes, :bx_resource_branches
 
   def setup
     User.current = User.find(1)
@@ -54,7 +54,6 @@ class BxResourceServiceTest < ActiveSupport::TestCase
     history = BxHistory.where(:target => "resource", :source_id => result.data.id).first
     assert_equal "create", history.operation_type
     assert_equal "create_root", history.operation
-    assert_equal result.data.id, history.source_id
     assert_equal 1, history.changed_by
     assert_equal now, history.changed_at
   end
@@ -63,7 +62,6 @@ class BxResourceServiceTest < ActiveSupport::TestCase
     form = BxRootResourceForm.new(:resource_code => "test", :resource_summary => "summary", :project => @project)
     result = BxResourceService.new(form).create_root!
     history = BxHistory.where(:target => "resource", :source_id => result.data.id).first
-    assert history.details.any? { |detail| detail.property == "summary"}
     code_detail = history.details.detect { |detail| detail.property == "code" }
     assert_not_nil code_detail
     assert_equal "", code_detail.old_value
@@ -85,6 +83,78 @@ class BxResourceServiceTest < ActiveSupport::TestCase
     form = BxRootResourceForm.new(:resource_code => "test", :resource_summary => "summary", :project => @project, :relational_issues => "1,3")
     result = BxResourceService.new(form).create_root!
     history = BxHistory.where(:target => "resource", :source_id => result.data.id).first
+    assert history.issues.any? { |issue| issue.id == 1 }
+    assert history.issues.any? { |issue| issue.id == 3 }
+  end
+
+  def test_branch_registration_on_add_branch
+    assert_difference("BxResourceBranch.count") do
+      resource = BxResourceNode.find(1)
+      form = BxResourceBranchForm.new(:code => "branch_test", :name => "branch_name", :root_node_id => resource.id)
+      result = BxResourceService.new(form).add_branch!
+      assert result.success?
+    end
+  end
+
+  def test_branch_not_registration_on_add_branch_by_invalid_form
+    assert_no_difference("BxResourceBranch.count") do
+      resource = BxResourceNode.find(1)
+      form = BxResourceBranchForm.new(:code => "", :name => "branch_name", :root_node_id => resource.id)
+      result = BxResourceService.new(form).add_branch!
+      assert result.invalid_input?
+    end
+  end
+
+  def test_registered_branch_values_on_add_branch
+    resource = BxResourceNode.find(1)
+    form = BxResourceBranchForm.new(:code => "branch_test", :name => "branch_name", :root_node_id => resource.id)
+    result = BxResourceService.new(form).add_branch!
+    branch = BxResourceBranch.where(:code => "branch_test", :root_node_id => resource.id).first
+    assert_not_nil branch
+    assert_equal "branch_name", branch.name
+  end
+
+  def test_history_values_on_add_branch
+    now = Time.now
+    Time.stubs(:now).returns(now)
+    resource = BxResourceNode.find(1)
+    form = BxResourceBranchForm.new(:code => "branch_test", :name => "branch_name", :root_node_id => resource.id)
+    result = BxResourceService.new(form).add_branch!
+    history = BxHistory.where(:target => "resource", :source_id => resource.id).last
+    assert_equal "create", history.operation_type
+    assert_equal "create_branch", history.operation
+    assert_equal 1, history.changed_by
+    assert_equal now, history.changed_at
+  end
+
+  def test_history_detail_values_on_add_branch
+    resource = BxResourceNode.find(1)
+    form = BxResourceBranchForm.new(:code => "branch_test", :name => "branch_name", :root_node_id => resource.id)
+    result = BxResourceService.new(form).add_branch!
+    history = BxHistory.where(:target => "resource", :source_id => resource.id).last
+    code_detail = history.details.detect { |detail| detail.property == "code" }
+    assert_not_nil code_detail
+    assert_equal "", code_detail.old_value
+    assert_equal "branch_test", code_detail.new_value
+    name_detail = history.details.detect { |detail| detail.property == "name" }
+    assert_not_nil name_detail
+    assert_equal "", name_detail.old_value
+    assert_equal "branch_name", name_detail.new_value
+  end
+
+  def test_relational_issue_registration_on_add_branch
+    assert_difference("BxHistoryIssue.count", 2) do
+      resource = BxResourceNode.find(1)
+      form = BxResourceBranchForm.new(:code => "branch_test", :name => "branch_name", :root_node_id => resource.id, :relational_issues => "1,3")
+      BxResourceService.new(form).add_branch!
+    end
+  end
+
+  def test_relational_issue_id_on_add_branch
+    resource = BxResourceNode.find(1)
+    form = BxResourceBranchForm.new(:code => "branch_test", :name => "branch_name", :root_node_id => resource.id, :relational_issues => "1,3")
+    result = BxResourceService.new(form).add_branch!
+    history = BxHistory.where(:target => "resource", :source_id => resource.id).first
     assert history.issues.any? { |issue| issue.id == 1 }
     assert history.issues.any? { |issue| issue.id == 3 }
   end
