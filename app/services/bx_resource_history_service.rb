a54deee84b746e72c07dd6daf1dcea9dbdaa6c75
pinzlo <pinzolo@gmail.com>
2013-08-26 15:43:14 +0900

    remove bx_evacuatable and use previous_changes.

--- app/services/bx_resource_history_service.rb
+++ app/services/bx_resource_history_service.rb
@@ -3,14 +3,17 @@ class BxResourceHistoryService < BxHistoryService
   target :resource
 
   def register_create_root_history(root, issue_ids)
-    self.register_history("create_root", root.id, root.changesets, issue_ids)
+    changesets = root.previous_changes.slice("code", "summary")
+    self.register_history("create_root", root.id, changesets, issue_ids)
   end
 
   def register_add_branch_history(branch, issue_ids)
-    self.register_history("create_branch", branch.root_node_id, branch.changesets, issue_ids)
+    changesets = branch.previous_changes.slice("code", "name")
+    self.register_history("create_branch", branch.root_node_id, changesets, issue_ids)
   end
 
   def register_update_branch_history(branch, issue_ids)
-    self.register_history("update_branch", branch.root_node_id, branch.changesets, issue_ids)
+    changesets = branch.previous_changes.slice("code", "name")
+    self.register_history("update_branch", branch.root_node_id, changesets, issue_ids)
   end
 end
