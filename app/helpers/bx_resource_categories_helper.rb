f1dafefebc9e2a42c01589e8c55bcb72cb10da9d
pinzlo <pinzolo@gmail.com>
2013-08-28 13:44:28 +0900

    display history details and relational issue links at category show page.

--- app/helpers/bx_resource_categories_helper.rb
+++ app/helpers/bx_resource_categories_helper.rb
@@ -2,9 +2,8 @@
 module BxResourceCategoriesHelper
   include BxHelper
 
-  def bx_history_details_link(history)
-    return "" if history.operation_type == "delete"
-
-    " (#{content_tag(:a, l("bx.label.history.details"), :href => "#", :class => "bx_history_details_link")})".html_safe
+  def bx_history_details_note(history, detail)
+    prop = l("bx.label.history_detail.properties.#{history.operation}.#{detail.property}")
+    l("bx.text.history_detail_notes.#{history.operation_type}", :property => prop, :new_value => detail.new_value, :old_value => detail.old_value).html_safe
   end
 end
--- assets/javascripts/redmine_bx.js
+++
@@ -1,3 +0,0 @@
-$(".bx_history_details_link").on("click", function() {
-  alert("hoge");
-});
