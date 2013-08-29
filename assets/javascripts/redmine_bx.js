5a1c96386a5206ce34bffffebe45448706c0b001
pinzolo <pinzolo@gmail.com>
2013-08-29 23:59:05 +0900

    add root resource style

---
+++ assets/javascripts/redmine_bx.js
@@ -0,0 +1,4 @@
+$(".bx_resources li").hover(
+    function() { $(this).addClass("bx_hover"); },
+    function() { $(this).removeClass("bx_hover"); }
+);
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
     get :bx, :controller => :bx_menu, :action => :index
 
     namespace :bx, :module => nil do
-      resources :resources, :controller => :bx_resources do
+      resources :resources, :controller => :bx_resources, :except => [:new, :create] do
         member do
           get :children
         end
       end
       resources :categories, :controller => :bx_resource_categories, :except => [:index] do
+        resources :resources, :controller => :bx_resources, :only => [:new, :create]
         resources :branches, :controller => :bx_resource_branches, :only => [:new, :create]
       end
       resources :branches, :controller => :bx_resource_branches, :only => [:edit, :update, :destroy]
