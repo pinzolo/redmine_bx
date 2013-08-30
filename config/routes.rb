3e5ac29b91db73bc245dae600361e4329f9488bf
pinzlo <pinzolo@gmail.com>
2013-08-30 15:20:43 +0900

    simplified bx_resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
     get :bx, :controller => :bx_menu, :action => :index
 
     namespace :bx, :module => nil do
-      resources :resources, :controller => :bx_resources, :except => [:new, :create] do
+      resources :resources, :controller => :bx_resources do
         member do
           get :children
         end
       end
       resources :categories, :controller => :bx_resource_categories, :except => [:index] do
-        resources :resources, :controller => :bx_resources, :only => [:new, :create]
         resources :branches, :controller => :bx_resource_branches, :only => [:new, :create]
       end
       resources :branches, :controller => :bx_resource_branches, :only => [:edit, :update, :destroy]
