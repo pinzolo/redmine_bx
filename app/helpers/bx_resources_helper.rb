ed025adb2b32cd10d81f72750c98795eb39a51c1
pinzlo <pinzolo@gmail.com>
2013-09-04 20:00:14 +0900

    bugfix: if resource has BxResourceValue that value is blank, marker was changed green to red.

--- app/helpers/bx_resources_helper.rb
+++ app/helpers/bx_resources_helper.rb
@@ -3,9 +3,10 @@ module BxResourcesHelper
   include BxHelper
 
   def bx_resource_mark(resource)
-    color_class = if resource.values.empty?
+    not_blank_value_count = resource.values.to_a.count { |value| value.value.present? }
+    color_class = if not_blank_value_count.zero?
                     "bx-resource-values-empty"
-                  elsif resource.values.length == resource.branches.length
+                  elsif not_blank_value_count == resource.branches.length
                     "bx-resource-values-full"
                   else
                     "bx-resource-values-partiality"
