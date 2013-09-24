6483caf3bf5ec7121c4e9553233439bcdc2c9df4
pinzolo <pinzolo@gmail.com>
2013-09-24 16:26:44 +0900

    add default_value and primary_key_number to bx_common_column_defs.

--- db/migrate/013_create_bx_common_column_defs.rb
+++ db/migrate/013_create_bx_common_column_defs.rb
@@ -12,7 +12,9 @@ class CreateBxCommonColumnDefs < ActiveRecord::Migration
       t.integer :size, :null => false, :default => 0
       t.integer :scale, :null => false, :default => 0
       t.boolean :nullable, :null => false, :default => false
+      t.string :default_value, :null => false, :default => ""
       t.integer :reference_column_id, :null => false, :default => 0
+      t.integer :primary_key_number, :null => true, :default => 0
       t.string :type, :null => false, :default => ""
       t.integer :position, :null => false, :default => 0
       t.text :note, :null => false, :default => ""
