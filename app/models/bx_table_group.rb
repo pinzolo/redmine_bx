83ae2d7c47fbd89800894bedb71cfc7d0e0caaf0
pinzolo <pinzolo@gmail.com>
2013-09-26 11:51:44 +0900

    add table group show page.

--- app/models/bx_table_group.rb
+++ app/models/bx_table_group.rb
@@ -2,7 +2,10 @@
 class BxTableGroup < ActiveRecord::Base
   unloadable
 
+  belongs_to :database, :class_name => "BxDatabase", :foreign_key => :database_id
   has_many :table_defs, :class_name => "BxTableDef", :foreign_key => :table_group_id, :order => :physical_name
   has_many :data_types_rels, :class_name => "BxTableGroupDataType", :foreign_key => :table_group_id
   has_many :data_types, :through => :data_types_rels, :order => :name
+  has_many :histories, :class_name => "BxHistory", :foreign_key => :source_id,
+                       :conditions => Proc.new { ["target = ?", "table_group"] }, :include => :details
 end
--- app/services/bx_table_def_history_service.rb
+++ app/services/bx_table_def_history_service.rb
@@ -1,8 +1,13 @@
 # coding: utf-8
 class BxTableDefHistoryService < BxHistoryService
   def register_create_table_group_history(table_group, issue_ids)
-    data_types = table_group.data_types.map(&:name).join(",")
-    changesets = table_group.previous_changes.slice("name", "description").merge("data_types" => ["", data_types])
+    changesets = table_group.previous_changes.slice("name", "database_id", "description")
+    if changesets.key?("database_id")
+      changesets["database"] = ["", table_group.database.name]
+      changesets.delete("database_id")
+    end
+    data_types = table_group.data_types.map(&:name).join(", ")
+    changesets["data_types"] = ["", data_types]
     self.register_history("table_group", "create_table_group", table_group.name, table_group.id, changesets, issue_ids)
   end
 end
