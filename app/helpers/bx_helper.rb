112ecd9ecd459f770faa2b8c94eb1f82cc3cd266
pinzolo <pinzolo@gmail.com>
2013-10-08 13:04:01 +0900

    unified history detail property locals(create and update)

--- app/helpers/bx_helper.rb
+++ app/helpers/bx_helper.rb
@@ -38,7 +38,8 @@ module BxHelper
   end
 
   def bx_history_details_note(history, detail)
-    prop = l("bx.label.history_detail.properties.#{history.operation}.#{detail.property}")
+    operation_target = history.operation.gsub(/\A(create|update)_/, "")
+    prop = l("bx.label.history_detail.properties.#{operation_target}.#{detail.property}")
     l("bx.text.history_detail_notes.#{history.operation_type}", :property => prop, :new_value => detail.new_value, :old_value => detail.old_value).html_safe
   end
 
