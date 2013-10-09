f84ff2fc10aaec82c5c9864dce35321216933ac4
pinzolo <pinzolo@gmail.com>
2013-10-09 10:43:51 +0900

    modified column name to bx_index_def.note from bx_index_def.description

--- db/migrate/016_create_bx_index_defs.rb
+++ db/migrate/016_create_bx_index_defs.rb
@@ -9,7 +9,7 @@ class CreateBxIndexDefs < ActiveRecord::Migration
       t.string :logical_name, :null => false, :default => ""
       t.string :physical_name, :null => false, :default => ""
       t.boolean :unique, :null => false, :default => false
-      t.text :description, :null => false, :default => ""
+      t.text :note, :null => false, :default => ""
     end
 
     add_index :bx_index_defs, :table_def_id
