6948710023124652de1169e3163a93d9fe495677
pinzolo <pinzolo@gmail.com>
2013-10-09 10:40:07 +0900

    modified attribute to index_def_def_id from index_id

--- app/models/bx_index_columns.rb
+++ app/models/bx_index_columns.rb
@@ -3,5 +3,5 @@ class BxIndexColumns < ActiveRecord::Base
   unloadable
 
   belongs_to :column_def, :class_name => "BxColumnDef", :foreign_key => :column_def_id
-  belongs_to :index_def, :class_name => "BxIndexDef", :foreign_key => :index_id
+  belongs_to :index_def, :class_name => "BxIndexDef", :foreign_key => :index_def_id
 end
--- db/migrate/017_create_bx_index_columns.rb
+++ db/migrate/017_create_bx_index_columns.rb
@@ -5,12 +5,12 @@ class CreateBxIndexColumns < ActiveRecord::Migration
       t.integer :lock_version, :null => false, :default => 0
       t.timestamps
 
-      t.integer :index_id, :null => false, :default => 0
+      t.integer :index_def_id, :null => false, :default => 0
       t.integer :column_def_id, :null => false, :default => 0
     end
 
-    add_index :bx_index_columns, :index_id
+    add_index :bx_index_columns, :index_def_id
     add_index :bx_index_columns, :column_def_id
-    add_index :bx_index_columns, [:index_id, :column_def_id], :unique => true
+    add_index :bx_index_columns, [:index_def_id, :column_def_id], :unique => true
   end
 end
