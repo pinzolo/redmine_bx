ee7d0b2f2be1bc3340c5d3e027a3e483e1ead6f4
pinzolo <pinzolo@gmail.com>
2013-10-09 10:40:27 +0900

    modified attribute to index_def_def_id from index_id

--- app/models/bx_index_def.rb
+++ app/models/bx_index_def.rb
@@ -2,6 +2,6 @@
 class BxIndexDef < ActiveRecord::Base
   unloadable
 
-  has_many :columns_rels, :class_name => "BxIndexColumn", :foreign_key => :index_id
+  has_many :columns_rels, :class_name => "BxIndexColumn", :foreign_key => :index_def_id
   has_many :column_defs, :through => :columns_rels
 end
