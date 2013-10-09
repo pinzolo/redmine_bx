6add2df2ec8444dd1cebd1faf6e7481683007b81
pinzolo <pinzolo@gmail.com>
2013-10-09 10:42:01 +0900

    added position column to bx_index_columns

--- db/migrate/017_create_bx_index_columns.rb
+++ db/migrate/017_create_bx_index_columns.rb
@@ -7,10 +7,12 @@ class CreateBxIndexColumns < ActiveRecord::Migration
 
       t.integer :index_def_id, :null => false, :default => 0
       t.integer :column_def_id, :null => false, :default => 0
+      t.integer :position, :null => false, :default => 0
     end
 
     add_index :bx_index_columns, :index_def_id
     add_index :bx_index_columns, :column_def_id
     add_index :bx_index_columns, [:index_def_id, :column_def_id], :unique => true
+    add_index :bx_index_columns, [:index_def_id, :position], :unique => true
   end
 end
