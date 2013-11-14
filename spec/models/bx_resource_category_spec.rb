45bb90727c11712cbca858a8b42a0787d94f6362
pinzolo <pinzolo@gmail.com>
2013-11-14 16:37:48 +0900

    Add spec for BxResourceCategory

---
+++ spec/models/bx_resource_category_spec.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe BxResourceCategory do
+  fixtures :projects, :bx_resource_categories, :bx_resource_branches, :bx_resource_nodes, :bx_resource_values
+
+  let(:category) { BxResourceCategory.find(1) }
+
+  describe "#resources" do
+    it "returns resource nodes that category_id is self and prent_id is 0 and ordered by path attribute" do
+      expected = BxResourceNode.where(:category_id => category.id, :parent_id => 0).order(:path).to_a
+      expect(category.resources.to_a).to eq expected
+    end
+  end
+
+  describe "#all_resources" do
+    it "returns resource nodes that category_id is self id and orderd by path attribute" do
+      expected = BxResourceNode.where(:category_id => category.id).order(:path).to_a
+      expect(category.all_resources.to_a).to eq expected
+    end
+  end
+
+  describe "#histories" do
+    it "returns histories that source_id is self and target is 'resource_category' and orderd by changed_at" do
+      expected = BxHistory.where(:source_id => category.id, :target => "resource_category").order(:changed_at).to_a
+      expect(category.histories.to_a).to eq expected
+    end
+  end
+end
+
