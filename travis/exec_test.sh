43b585885e46eac0e651317367d2dd7766b9df27
pinzolo <pinzolo@gmail.com>
2013-11-22 21:36:14 +0900

    Fix test can't execute

--- travis/exec_test.sh
+++ travis/exec_test.sh
@@ -8,9 +8,8 @@ bundle exec rake generate_secret_token
 bundle exec rake db:migrate
 bundle exec rake redmine:load_default_data
 
-# Copy assets
-# Plugin's migration is already done at db:migrate task
-bundle exec rake redmine:plugins:assets NAME=redmine_bx
+# Copy assets & execute plugin's migration
+bundle exec rake redmine:plugins NAME=redmine_bx
 
 # Initialize RSpec
 bundle exec rails g rspec:install
