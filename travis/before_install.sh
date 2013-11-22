03a2a74407adcb577b0a460adf863954e8c109e0
pinzolo <pinzolo@gmail.com>
2013-11-22 20:33:05 +0900

    Modify before_install.sh

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -6,28 +6,30 @@ REDMINE_VERSION="2.4.0"
 wget http://rubyforge.org/frs/download.php/77242/redmine-${REDMINE_VERSION}.tar.gz
 tar zxf redmine-${REDMINE_VERSION}.tar.gz
 
-mv --help
+ls -al redmine-${REDMINE_VERSION}
+pwd
 
 # copy plugin files
-cd redmine-${REDMINE_VERSION}
-mkdir /plugins/redmine_bx
-cp -r ../app     plugins/redmine_bx/app
-cp -r ../assets  plugins/redmine_bx/assets
-cp -r ../config  plugins/redmine_bx/config
-cp -r ../db      plugins/redmine_bx/db
-cp -r ../lib     plugins/redmine_bx/lib
-cp -r ../spec    plugins/redmine_bx/spec
-cp -r ../test    plugins/redmine_bx/test
-cp ../Gemfile    plugins/redmine_bx/Gemfile
-cp ../init.rb    plugins/redmine_bx/init.rb
+#cd redmine-${REDMINE_VERSION}
+mkdir redmine-${REDMINE_VERSION}/plugins/redmine_bx
+mv ../app      redmine-${REDMINE_VERSION}/plugins/redmine_bx/app
+mv ../assets   redmine-${REDMINE_VERSION}/plugins/redmine_bx/assets
+mv ../config   redmine-${REDMINE_VERSION}/plugins/redmine_bx/config
+mv ../db       redmine-${REDMINE_VERSION}/plugins/redmine_bx/db
+mv ../lib      redmine-${REDMINE_VERSION}/plugins/redmine_bx/lib
+mv ../spec     redmine-${REDMINE_VERSION}/plugins/redmine_bx/spec
+mv ../test     redmine-${REDMINE_VERSION}/plugins/redmine_bx/test
+mv ../Gemfile  redmine-${REDMINE_VERSION}/plugins/redmine_bx/Gemfile
+mv ../init.rb  redmine-${REDMINE_VERSION}/plugins/redmine_bx/init.rb
 
 # create files
-cat > config/database.yml <<_EOS_
+cat > redmine-${REDMINE_VERSION}/config/database.yml <<_EOS_
 test:
   adapter: sqlite3
   database: db/redmine_test.db
 _EOS_
-cp plugins/redmine_bx/test/fixtures/* test/fixtures/
+cp redmine-${REDMINE_VERSION}/plugins/redmine_bx/test/fixtures/* redmine-${REDMINE_VERSION}/test/fixtures/
 
-export BUNDLE_GEMFILE=./Gemfile
+echo $PWD
+export BUNDLE_GEMFILE=redmine-${REDMINE_VERSION}/Gemfile
 
