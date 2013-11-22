8f57438622da0540b4859ed8548c454d54e6b87a
pinzolo <pinzolo@gmail.com>
2013-11-22 20:51:53 +0900

    Modify travis files

--- travis/exec_test.sh
+++ travis/exec_test.sh
@@ -7,4 +7,5 @@ bundle exec rake generate_secret_token
 bundle exec rake db:migrate
 bundle exec rake redmine:load_default_data
 bundle exec rake redmine:plugins NAME=redmine_bx
+bundle exec rails g rspec:install
 
