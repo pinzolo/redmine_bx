f0d29a24adaa5042cd6029f65f28472ca2b87f19
pinzolo <pinzolo@gmail.com>
2013-11-28 13:09:32 +0900

    Modify simplecov filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,11 @@ require "coveralls"
 require "simplecov"
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/bundle/'
+  add_filter do |source_file|
+    !source_file.filename.include?("redmine_bx/app") || !source_file.filename.end_with?(".rb")
+  end
 end
 
-
-# change fixture_path in base spec_helper.rb
-#   from: config.fixture_path = "#{::Rails.root}/spec/fixtures"
-#   to  : config.fixture_path = "#{::Rails.root}/test/fixtures"
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec/spec_helper')
 
 RSpec.configure do |config|
