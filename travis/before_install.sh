e55fc253a552dfdd9d4a5a4e59a8663777c4b0c4
pinzolo <pinzolo@gmail.com>
2013-11-29 17:44:17 +0900

    Refactoring before_install.sh

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,23 +1,23 @@
 #! /bin/sh
 
 REDMINE_VERSION="2.4.1"
+PLUGIN_NAME="redmine_bx"
 
 # Get & deploy Redmine
 wget http://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz
-#wget http://rubyforge.org/frs/download.php/77242/redmine-${REDMINE_VERSION}.tar.gz
 tar zxf redmine-${REDMINE_VERSION}.tar.gz
 
 # Copy plugin files to plugin directory
-mkdir redmine-${REDMINE_VERSION}/plugins/redmine_bx
-mv app      redmine-${REDMINE_VERSION}/plugins/redmine_bx/app
-mv assets   redmine-${REDMINE_VERSION}/plugins/redmine_bx/assets
-mv config   redmine-${REDMINE_VERSION}/plugins/redmine_bx/config
-mv db       redmine-${REDMINE_VERSION}/plugins/redmine_bx/db
-mv lib      redmine-${REDMINE_VERSION}/plugins/redmine_bx/lib
-mv spec     redmine-${REDMINE_VERSION}/plugins/redmine_bx/spec
-mv test     redmine-${REDMINE_VERSION}/plugins/redmine_bx/test
-mv Gemfile  redmine-${REDMINE_VERSION}/plugins/redmine_bx/Gemfile
-mv init.rb  redmine-${REDMINE_VERSION}/plugins/redmine_bx/init.rb
+mkdir redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}
+mv app      redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/app
+mv assets   redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/assets
+mv config   redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/config
+mv db       redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/db
+mv lib      redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/lib
+mv spec     redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/spec
+mv test     redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/test
+mv Gemfile  redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/Gemfile
+mv init.rb  redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/init.rb
 
 # Create necessary files
 cat > redmine-${REDMINE_VERSION}/config/database.yml <<_EOS_
@@ -25,7 +25,7 @@ test:
   adapter: sqlite3
   database: db/redmine_test.db
 _EOS_
-cp redmine-${REDMINE_VERSION}/plugins/redmine_bx/test/fixtures/* redmine-${REDMINE_VERSION}/test/fixtures/
+cp redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/test/fixtures/* redmine-${REDMINE_VERSION}/test/fixtures/
 
 # All move to work directory
 mv redmine-${REDMINE_VERSION}/* .
