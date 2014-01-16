172b9281f2caa0e8d852b39fd8af446cdefe8609
pinzolo <pinzolo@gmail.com>
2014-01-16 15:41:52 +0900

    Remove 'test' directory

--- test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-# Load the Redmine helper
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-
-#class ActiveSupport::TestCase
-#  self.fixture_path = File.dirname(__FILE__) + '/fixtures'
-#end
--- test/unit/bx_resource_branch_test.rb
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-require File.expand_path('../../test_helper', __FILE__)
-
-class BxResourceBranchTest < ActiveSupport::TestCase
-end
