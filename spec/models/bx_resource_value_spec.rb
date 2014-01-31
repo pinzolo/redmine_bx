152e74c27f7f36b176bd0f5fb1b4a0ab29c13e61
pinzolo <pinzolo@gmail.com>
2014-01-31 17:46:56 +0900

    Fix typo

--- spec/models/bx_resource_value_spec.rb
+++ spec/models/bx_resource_value_spec.rb
@@ -2,7 +2,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe BxResourceValue do
-  fixtures :projects, :bx_resource_branch, :bx_resource_nodes, :bx_resource_values
+  fixtures :projects, :bx_resource_branches, :bx_resource_nodes, :bx_resource_values
   let(:resource_value) { BxResourceValue.find(1) }
 
   describe "#resource" do
